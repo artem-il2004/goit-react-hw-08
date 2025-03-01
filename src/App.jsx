bb640ae387d7b164fcf570ab827d42d49722cf10
artem <artemilchenko28@gmail.com>
2025-03-01 19:49:54 +0100

    i

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { Outlet, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import ContactsPage from "./pages/ContactsPage/ContactsPage";
-import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import { lazy, Suspense } from "react"; 
 import Layout from "./components/Layout/Layout";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
@@ -12,26 +8,36 @@ import { selectIsRefresh } from "./redux/auth/selectors";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute";
 
+// Ленивая загрузка компонентов
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
+const RegistrationPage = lazy(() =>
+  import("./pages/RegistrationPage/RegistrationPage")
+);
+
 function App() {
-    const dispatch = useDispatch();
-    const isRefreshing = useSelector(selectIsRefresh);
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefresh);
 
-    useEffect(() => {
-        dispatch(refreshUser());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
 
-    return isRefreshing ? null : (
-        <Routes>
-  <Route path="/" element={<Layout><Outlet /></Layout>}>
-    <Route index element={<HomePage />} />
-    <Route path="/contacts" element={<PrivateRoute component={ContactsPage} />} />
-    <Route path="/login" element={<RestrictedRoute component={LoginPage} />} />
-    <Route path="/register" element={<RestrictedRoute component={RegistrationPage} />} />
-    <Route path="*" element={<NotFoundPage />} />
-  </Route>
-</Routes>
-
-    );
+  return isRefreshing ? null : (
+    <Suspense fallback={<div>Loading...</div>}> 
+      <Routes>
+        <Route path="/" element={<Layout><Outlet /></Layout>}>
+          <Route index element={<HomePage />} />
+          <Route path="/contacts" element={<PrivateRoute component={ContactsPage} />} />
+          <Route path="/login" element={<RestrictedRoute component={LoginPage} />} />
+          <Route path="/register" element={<RestrictedRoute component={RegistrationPage} />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
